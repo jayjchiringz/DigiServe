65878f6bb7cda4054d5406512c2be3fdadd25e0d
jayjchiringz <jayjchiringz@gmail.com>
2025-04-16 17:31:56 +0300

    Working Latest Repo from local

--- DigiServe/settings.py
+++ DigiServe/settings.py
@@ -29,7 +29,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'https://127.0.0.1:8000',
-    'https://technothrone.pythonanywhere.com',
+    'https://DigiServe25.pythonanywhere.com',
 ]
 
 # Set up logging for CSRF failures
